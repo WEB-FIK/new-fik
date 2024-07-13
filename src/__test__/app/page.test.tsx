dccecb180df1ef526126910effaef43bdebcda26
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-07-13 12:52:55 +0700

    mengganti teks dengan output yang benar

--- src/__test__/app/page.test.tsx
+++ src/__test__/app/page.test.tsx
@@ -5,6 +5,6 @@ import Home from "../../app/page";
 describe("Home", () => {
   it("renders without crashing", () => {
     render(<Home />);
-    expect(screen.getByText("Get started by Editing")).toBeInTheDocument();
+    expect(screen.getByText("Get started by editing")).toBeInTheDocument();
   });
 });
