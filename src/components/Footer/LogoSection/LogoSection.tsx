dd4113a53b24793072e14df6ab875fc4e852f388
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-07-24 20:59:09 +0700

    Fixing Image Directory for LogoSection.tsx in Footer

--- src/components/Footer/LogoSection/LogoSection.tsx
+++ src/components/Footer/LogoSection/LogoSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import upnvj from "../../../public/icons/UPNVJ.png";
+import upnvj from "../../../../public/icons/UPNVJ.png";
 
 const LogoSection = () => (
   <div className="flex items-center space-x-4 md:pt-9">
