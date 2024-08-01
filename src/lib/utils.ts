0d0f2ecdf4074a45e675a3a0186973a94571bc4f
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-01 13:25:46 +0700

    Adding truncateText function to shortened the description for example

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function truncateText(text: string, wordLimit: number) {
+  const words = text.split(" ");
+  if (words.length > wordLimit) {
+    return words.slice(0, wordLimit).join(" ") + "...";
+  }
+  return text;
+}
