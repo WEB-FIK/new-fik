26fde44d40569f07f44c56cdaf3b51167ba59c1b
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-16 21:13:35 +0700

    Update KehidupanMahasiswaCard.tsx
    Revising the classNames and fixing the variables placement

--- src/components/ui/KehidupanMahasiswaCard/KehidupanMahasiswaCard.tsx
+++ src/components/ui/KehidupanMahasiswaCard/KehidupanMahasiswaCard.tsx
@@ -8,28 +8,35 @@ import { truncateText } from "@/lib/utils";
 const KehidupanMahasiswaCard: React.FC<{
   data: KehidupanMahasiswaCardProps;
 }> = ({ data }) => {
+  const truncatedDescription = truncateText(data.description, 30);
+
+  const renderImage = data.image && (
+    <Image
+      src={data.image}
+      alt={data.title}
+      className="px-28 py-12 object-coverrounded-[8px]"
+      layout="fill"
+    />
+  );
+
   return (
-    <div className="w-full min-h-[700px] lg:min-h-[100px] rounded-[8px] p-4 border border-slate-200 space-y-6 hover:border-primary-500 transform transition-all ease-in-out duration-300 hover:shadow-lg">
-      <div className="w-full flex justify-center items-center min-h-[340px] rounded-[16px] border border-slate-200">
-        <Image
-          src={data.image}
-          alt={data.title}
-          className="object-cover rounded-[8px]"
-        />
+    <div className="w-full min-h-[700px] lg:min-h-[100px] rounded-lg p-4 border border-slate-200 space-y-6 hover:border-primary-500 transform transition-all ease-in-out duration-300 hover:shadow-lg">
+      <div className="w-full flex justify-center items-center min-h-80 rounded-2xl border border-slate-200 relative">
+        {renderImage}
       </div>
       <div className="flex flex-col justify-between space-y-6">
         <p className="text-[26px] font-bold">{data.title}</p>
 
         <p className="text-neutral-500 text-[18px] font-bold text-justify">
-          {truncateText(data.description, 30)}
+          {truncatedDescription}
         </p>
+
         <div className="flex items-center space-x-2">
-          <InstagramIcon />
-          <Link
-            href={data.link}
-            className="text-primary-500 text-[20px] font-bold"
-          >
-            {data.linkTitle}
+          <InstagramIcon aria-label="Instagram Icon" />
+          <Link href={data.link}>
+            <p className="text-primary-500 text-[20px] font-bold focus:outline-none focus:ring-2 focus:ring-primary-500">
+              {data.linkTitle}
+            </p>
           </Link>
         </div>
       </div>
