327acbef809539379dcaa91107783d1f471a141b
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-20 06:47:38 +0700

    Update StrukturManajemenCard.tsx

--- src/components/ui/StrukturManajemenCard/StrukturManajemenCard.tsx
+++ src/components/ui/StrukturManajemenCard/StrukturManajemenCard.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import { LuBookMarked } from "react-icons/lu";
-
+import StrukturManajemenCardLayout from "./StrukturManajemenCardLayout";
 interface StrukturManajemenCardProps {
   image: StaticImageData;
   name: string;
@@ -13,49 +13,40 @@ interface StrukturManajemenCardProps {
   profileRoute?: string;
   lhkpnRoute?: string;
 }
-
 const StrukturManajemenCard: React.FC<StrukturManajemenCardProps> = ({
   image,
   name,
   position,
   role,
-  showMenu,
+  showMenu = false,
   profileRoute,
   lhkpnRoute,
 }) => {
   return (
-    <div className="relative rounded-[16px] h-[400px] w-[360px] overflow-hidden group">
-      <Image
-        src={image}
-        alt={name}
-        className="rounded-[16px] w-full h-full object-cover transition-transform duration-500 group-hover:scale-[108%] cursor-pointer"
-      />
-      <div className="absolute inset-0 bg-gradient-to-t hover:rounded-[16px] from-primary-500 via-transparent to-transparent opacity-0 group-hover:opacity-70 transition-opacity duration-500 pointer-events-none"></div>
-      <div className="absolute bottom-0 left-0 right-0 p-4 text-center text-white space-y-3 px-3 py-4">
-        <p className="text-[18px] font-bold">{name}</p>
-        <p className="text-[18px] font-bold">{position}</p>
-      </div>
-      {showMenu && (
-        <div className="absolute bottom-0 left-0 right-0 flex justify-around items-center bg-primary-500 bg-opacity-100 h-[100px] text-white py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-          <Link
-            href={profileRoute || "#"}
-            className="text-[16px] font-bold flex items-center gap-x-2 hover:underline"
-          >
-            <CgProfile className="text-[32px]" />
-            Profile
-          </Link>
+    <StrukturManajemenCardLayout image={image} name={name} position={position}>
+      {showMenu && (profileRoute || lhkpnRoute) && (
+        <footer className="absolute bottom-0 left-0 right-0 flex justify-around items-center bg-primary-500 h-24 text-white py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+          {profileRoute && (
+            <Link
+              href={profileRoute}
+              className="text-base font-bold flex items-center gap-x-2 hover:underline"
+            >
+              <CgProfile className="text-2xl" />
+              Profile
+            </Link>
+          )}
           {lhkpnRoute && (
             <Link
               href={lhkpnRoute}
-              className="text-[16px] font-bold flex items-center gap-x-2 hover:underline"
+              className="text-base font-bold flex items-center gap-x-2 hover:underline"
             >
-              <LuBookMarked className="text-[32px]" />
+              <LuBookMarked className="text-2xl" />
               LHKPN
             </Link>
           )}
-        </div>
+        </footer>
       )}
-    </div>
+    </StrukturManajemenCardLayout>
   );
 };
 
