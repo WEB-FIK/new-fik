d7402fac328e12d348c834c393b897984d5a8b80
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-19 12:14:32 +0700

    Update SectionHeader.tsx

--- src/components/ui/SectionHeader/SectionHeader.tsx
+++ src/components/ui/SectionHeader/SectionHeader.tsx
@@ -15,36 +15,29 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({
   aosAnimation = "fade-down",
   className = "",
 }) => {
-  const alignmentClasses = `text-${textAlign}`;
+  const baseClasses = "flex flex-col";
+  const alignmentClasses = `items-${textAlign} text-${textAlign}`;
   const layoutClasses = textAlign === "right" ? "flex-row-reverse" : "flex-row";
-  const maxWidth = "max-w-[50%]";
-  const justifyContent =
+  const maxWidthClasses = "max-w-[50%]";
+  const justifyContentClasses =
     textAlign === "center" ? "justify-center" : "justify-between";
 
+  const containerClasses = `${baseClasses} ${alignmentClasses} ${className}`;
+  const titleClasses = `text-[24px] font-extrabold md:text-[48px] ${maxWidthClasses} ${alignmentClasses}`;
+  const hrClasses = `border-primary-500 rounded-xl border-[3px] flex-grow ${
+    textAlign === "center" ? "w-1/2" : maxWidthClasses
+  }`;
+  const subtitleClasses = `text-primary-500 text-[16px] font-semibold md:text-[24px] mt-2 ${alignmentClasses}`;
+
   return (
-    <div
-      className={`flex flex-col items-${textAlign} ${alignmentClasses} ${className}`}
-      data-aos={aosAnimation}
-    >
+    <div className={containerClasses} data-aos={aosAnimation}>
       <div
-        className={`flex items-center ${justifyContent} gap-x-8 ${layoutClasses}`}
+        className={`flex items-center ${justifyContentClasses} gap-x-8 ${layoutClasses}`}
       >
-        <p
-          className={`text-[24px] font-extrabold md:text-[48px] ${maxWidth} ${alignmentClasses}`}
-        >
-          {title}
-        </p>
-        <hr
-          className={`border-primary-500 rounded-xl border-[3px] flex-grow ${
-            textAlign === "center" ? "w-1/2" : maxWidth
-          }`}
-        />
+        <p className={titleClasses}>{title}</p>
+        <hr className={hrClasses} />
       </div>
-      <p
-        className={`text-primary-500 text-[16px] font-semibold md:text-[24px] mt-2 ${alignmentClasses}`}
-      >
-        {subtitle}
-      </p>
+      <p className={subtitleClasses}>{subtitle}</p>
     </div>
   );
 };
