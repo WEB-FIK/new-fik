f345222a0d5244bcce5c8631feb801e4147bc08c
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-29 20:49:50 +0700

    Update PeminatanCard.tsx

--- src/components/ui/PeminatanCard/PeminatanCard.tsx
+++ src/components/ui/PeminatanCard/PeminatanCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ImacIcon from '@/components/Icons/ImacIcon';
-import { PeminatanProgramStudi } from '@/data/peminatanProgramStudi';
+import React from "react";
+import ImacIcon from "@/components/Icons/ImacIcon";
+import { PeminatanProgramStudi } from "@/data/peminatanProgramStudi";
 
 interface PeminatanCardProps {
   data: PeminatanProgramStudi[];
@@ -8,15 +8,20 @@ interface PeminatanCardProps {
 
 const PeminatanCard: React.FC<PeminatanCardProps> = ({ data }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center w-full">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {data.map((peminatan, index) => (
-        <div key={index} className="shadow-lg w-full md:w-[400px] min-h-[280px] flex flex-col justify-center items-center px-5 py-4 rounded-[30px] bg-white mb-4">
+        <div
+          key={index}
+          className="shadow-lg w-full md:w-[400px] min-h-[280px] flex flex-col justify-center items-center px-5 py-4 rounded-[30px] bg-white mb-4"
+        >
           <div className="flex flex-col justify-center items-center gap-y-2">
             <div>
               <ImacIcon />
             </div>
             <div className="text-black text-center gap-y-2 flex flex-col">
-              <p className="text-[18px] font-bold text-primary-500">{peminatan.title}</p>
+              <p className="text-[18px] font-bold text-primary-500">
+                {peminatan.title}
+              </p>
               <p className="text-[14px]">{peminatan.subtitle}</p>
             </div>
           </div>
