bf37e8e7adcc391f46d84ad6376e9651c4bcaf5d
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-30 09:57:15 +0700

    Updating the Accordion for CPLProgramStudi

--- src/components/ui/Accordion/AccordionCPLProgramStudi.tsx
+++ src/components/ui/Accordion/AccordionCPLProgramStudi.tsx
@@ -1,30 +1,11 @@
+import React from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/Accordion/Accordion";
-
-const accordionDatas = [
-  {
-    id: 1,
-    title:
-      "Capaian Pembelajaran Program Studi Sistem Informasi Program Sarjana",
-    content: "Yes. It adheres to the WAI-ARIA design pattern.",
-  },
-  {
-    id: 2,
-    title: "Capaian Pelajaran Fakultas Ilmu Komputer UPNVJ",
-    content:
-      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
-  },
-  {
-    id: 3,
-    title: "Capaian Pelajaran UPNVJ",
-    content:
-      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
-  },
-];
+import { accordionDatas } from "@/data/dataCPL";
 
 export function AccordionProgramStudi() {
   return (
