05ecd1ffdf4b0b2f849c36028ade7b94bd2fde13
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-08-30 09:56:40 +0700

    Adding FAQ and CPL datas to separate file

---
+++ src/data/dataCPL.ts
@@ -0,0 +1,20 @@
+export const accordionDatas = [
+  {
+    id: 1,
+    title:
+      "Capaian Pembelajaran Program Studi Sistem Informasi Program Sarjana",
+    content: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: 2,
+    title: "Capaian Pelajaran Fakultas Ilmu Komputer UPNVJ",
+    content:
+      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
+  },
+  {
+    id: 3,
+    title: "Capaian Pelajaran UPNVJ",
+    content:
+      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
+  },
+];
---
+++ src/data/dataFAQ.ts
@@ -0,0 +1,19 @@
+export const faqDatas = [
+  {
+    id: 1,
+    title: "Apa Perbedaan Diploma dan Sarjana Sistem Informasi FIK UPNVJ?",
+    content: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: 2,
+    title: "Apa Perbedaan Diploma dan Sarjana Sistem Informasi FIK UPNVJ?",
+    content:
+      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
+  },
+  {
+    id: 3,
+    title: "Apa Perbedaan Diploma dan Sarjana Sistem Informasi FIK UPNVJ?",
+    content:
+      "Yes. It comes with default styles that match the other components&apos; aesthetic.",
+  },
+];
