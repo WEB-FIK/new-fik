2182be5886e6d9a5c096492ef8792aebfe8a961a
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-09-11 16:23:55 +0700

    Update IncomingAgenda.tsx

--- src/components/Agenda/IncomingAgenda/IncomingAgenda.tsx
+++ src/components/Agenda/IncomingAgenda/IncomingAgenda.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import SectionHeader from "@/components/ui/SectionHeader/SectionHeader";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "./styles.css"; // Ensure this file includes your Swiper styles and any custom styling
 import { FreeMode, Pagination, Autoplay } from "swiper/modules";
 import AgendaCard from "@/components/ui/AgendaCard/AgendaCard";
-import RektoratUPN from "../../../../public/images/RektoratUPN.webp";
 import { agendaItems } from "@/data/dataAgenda";
+import Link from "next/link";
 
 function IncomingAgenda() {
   return (
     <section className="w-full px-4 py-8 flex flex-col justify-center space-y-5 md:px-[100px] md:min-h-[400px]">
-      <SectionHeader title="Akan Datang" subtitle="" textAlign="left" />
+      <h2 className="text-3xl font-bold">Akan Datang</h2>
       <div>
         <Swiper
           slidesPerView={1}
@@ -25,34 +23,33 @@ function IncomingAgenda() {
             delay: 2500,
           }}
           breakpoints={{
-            // When the window is >= 640px
-            640: {
-              slidesPerView: 2,
-            },
-            // When the window is >= 768px
-            768: {
-              slidesPerView: 3,
-            },
-            // When the window is >= 1024px
-            1024: {
-              slidesPerView: 4,
-            },
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
           }}
           modules={[FreeMode, Pagination, Autoplay]}
           className="mySwiper w-full"
         >
-          {agendaItems.map((item, index) => (
-            <SwiperSlide key={index}>
-              <AgendaCard
-                image={item.imageSrc}
-                AgendaDate={item.AgendaDate}
-                AgendaMonth={item.AgendaMonth}
-                AgendaTitle={item.AgendaTitle}
-                AgendaHour={item.AgendaHour}
-                AgendaLocation={item.AgendaLocation}
-              />
-            </SwiperSlide>
-          ))}
+          {agendaItems.map((item, index) => {
+            // Generate slug from the agenda title
+            const slug = item.AgendaTitle.toLowerCase().replace(/\s+/g, "-");
+
+            return (
+              <SwiperSlide key={index}>
+                <div>
+                  <AgendaCard
+                    image={item.imageSrc}
+                    AgendaDate={item.AgendaDate}
+                    AgendaMonth={item.AgendaMonth}
+                    AgendaTitle={item.AgendaTitle}
+                    AgendaHour={item.AgendaHour}
+                    AgendaLocation={item.AgendaLocation}
+                    slug={item.slug}
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </div>
     </section>
